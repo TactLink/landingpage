644dc448cb135ec626f34f3ac285bfb88ded6897
MichaelWong <c.shin1509@gmail.com>
2025-09-10 08:06:24 +0800

    fix: hydration issue for privacy page

--- frontend/app/privacy/page.tsx
+++ frontend/app/privacy/page.tsx
@@ -30,13 +30,13 @@ export default function PrivacyPage() {
               <p>
                 <strong>"API"</strong> means application programming interfaces
                 including but not limited to:
-                <ul className="ml-6 mt-2 space-y-1">
-                  <li>Facebook – login;</li>
-                  <li>Google – login;</li>
-                  <li>Linkedin – login; and</li>
-                  <li>Firebase – Dynamic Links;</li>
-                </ul>
               </p>
+              <ul className="ml-6 mt-2 space-y-1">
+                <li>Facebook – login;</li>
+                <li>Google – login;</li>
+                <li>Linkedin – login; and</li>
+                <li>Firebase – Dynamic Links;</li>
+              </ul>
               <p>
                 <strong>"Tactlink Application"</strong> means our free or paid
                 software branded "TACTLINK©", including any web-based or
